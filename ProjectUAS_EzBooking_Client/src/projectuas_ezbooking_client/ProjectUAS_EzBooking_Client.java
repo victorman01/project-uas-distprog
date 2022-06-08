8fb0e3dd1127498b3fab7c85b7ac18054e420cf6
Muhammad Ikhsan <iksan8991@gmail.com>
2022-06-08 22:53:33 +0700

    FIle awal

---
+++ ProjectUAS_EzBooking_Client/src/projectuas_ezbooking_client/ProjectUAS_EzBooking_Client.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projectuas_ezbooking_client;
+
+/**
+ *
+ * @author Muhammad Ikhsan
+ */
+public class ProjectUAS_EzBooking_Client {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ ProjectUAS_EzBooking_Server/src/projectuas_ezbooking_server/ProjectUAS_EzBooking_Server.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projectuas_ezbooking_server;
+
+/**
+ *
+ * @author Muhammad Ikhsan
+ */
+public class ProjectUAS_EzBooking_Server {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
