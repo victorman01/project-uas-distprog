fa76ac4a0eab443b07134bc78f6087f84f8b58c5
Subrata8778 <93910718+Subrata8778@users.noreply.github.com>
2022-06-12 22:39:10 +0700

    Add class Administrator and Preorder

---
+++ ProjectUAS_EzBooking_Server/src/model/Preorder.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Asus
+ */
+public class Preorder {
+
+//    Reservasi reservasi;
+    Menu menu;
+    int jumlah;
+    Connection connect;
+
+    Statement stat;
+    ResultSet result;
+
+}
